d075355f7c7b69183ba369fa820d9cdbdcd7743f
baek009 <mjse9@naver.com>
2024-04-17 18:05:01 +0900

    feature : 슬라이드 업데이트

---
+++ public/js/topslide.js
@@ -0,0 +1,34 @@
+// html 을 다루려고 하는 목적인 경우
+window.addEventListener("load", function());{}
+    // 1. 외부에서 자료를 불러온다.
+    const dataUrl = "./apis/topslide.json";
+    fetch(dataUrl)
+    .then((response) => {
+      // Step 1. 자료 받아서 json 변경하기
+      // 토큰을 js의 데이터로 변경하기
+        const data = response.json();
+      // 변환된 결과를 돌려주기
+        return data;
+    })
+    .then((result) => {
+        // Step 2. json 변경된 데이터 활용하기
+    console.log(result[0]);
+    console.log(result[1]);
+    console.log(result[2]);
+    console.log(result[3]);
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+
+
+    // 2. 자료를 이용해서 슬라이드에 배치할 html 을 만든다.
+    // 3. html 완성후 swiper 를 생성한다.
+    
+    // 기본코드를 넣어보자.
+    var topslide = new Swiper(".topSlide", {
+});
+
+function gogo () {
+}
+const 함수 = function gogo(){}
